ad1c24518aa5938710bed7a57d157e5be9458443
unknown <rajovicmirko@gmail.com>
2020-07-31 17:41:30 +0200

    Addad initial pages

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,70 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- layout -->
+    <v-app id="inspire">
+      <v-navigation-drawer v-model="drawerRight" app clipped right>
+        <v-list dense> </v-list>
+      </v-navigation-drawer>
+
+      <v-app-bar app clipped-right color="primary" dark>
+        <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+        <v-toolbar-title>VideoChatMaster</v-toolbar-title>
+        <v-spacer></v-spacer>
+        <v-app-bar-nav-icon
+          @click.stop="drawerRight = !drawerRight"
+        ></v-app-bar-nav-icon>
+      </v-app-bar>
+
+      <v-navigation-drawer v-model="drawer" app>
+        <v-list dense>
+          <v-list-item @click.stop="handleLogout">
+            <v-list-item-action>
+              <v-icon>mdi-logout-variant</v-icon>
+            </v-list-item-action>
+            <v-list-item-content>
+              <v-list-item-title>Logout</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+
+          <!-- <v-list-item
+            v-for="route in this.$router.options.routes"
+            :key="route.name"
+            link
+          >
+            <v-list-item-action>
+              <v-icon>{{ route.icon }}</v-icon>
+            </v-list-item-action>
+            <v-list-item-content>
+              <v-list-item-title>{{ route.name }}</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item> -->
+        </v-list>
+      </v-navigation-drawer>
+
+      <v-main>
+        <v-container class="fill-height" fluid>
+          <router-view />
+        </v-container>
+      </v-main>
+    </v-app>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+  name: "Home",
+
+  data: () => ({
+    drawer: null,
+    drawerRight: null,
+  }),
+
+  methods: {
+    handleLogout() {
+      this.$router.push({
+        name: "Login",
+      });
+    },
+  },
+};
 </script>
---
+++ src/views/Register.vue
@@ -0,0 +1,66 @@
+<template>
+  <v-app id="inspire">
+    <v-main>
+      <v-container class="fill-height" fluid>
+        <v-row align="center" justify="center">
+          <v-col cols="12" sm="8" md="4">
+            <v-card class="elevation-12">
+              <v-toolbar color="primary" dark flat>
+                <v-toolbar-title>Register form</v-toolbar-title>
+                <v-spacer></v-spacer>
+                <router-link to="/login" style="color: white"
+                  >Login</router-link
+                >
+              </v-toolbar>
+              <v-card-text>
+                <v-form>
+                  <v-text-field
+                    label="Email"
+                    name="email"
+                    prepend-icon="mdi-email"
+                    type="text"
+                  ></v-text-field>
+
+                  <v-text-field
+                    label="Username"
+                    name="username"
+                    prepend-icon="mdi-account"
+                    type="text"
+                  ></v-text-field>
+
+                  <v-text-field
+                    id="password"
+                    label="Password"
+                    name="password"
+                    prepend-icon="mdi-lock"
+                    type="password"
+                  ></v-text-field>
+                </v-form>
+              </v-card-text>
+              <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn color="primary" @click.stop="handleRegister"
+                  >Regiser</v-btn
+                >
+              </v-card-actions>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: "Register",
+
+  methods: {
+    handleRegister() {
+      this.$router.push({
+        name: "Home",
+      });
+    },
+  },
+};
+</script>
